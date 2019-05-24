5ae0f612d64c6e9c6979eb0e4fe704e99cfe58ae
Alan Dixon <alan.g.dixon@gmail.com>
2019-05-24 16:52:50 -0400

    updates to work with Simuliidae

--- run.sh
+++ run.sh
@@ -7,18 +7,17 @@ die () {
 
 syslogd
 
-PUID=${PUID:-3050}
-PGID=${PGID:-3050}
+PUID=${PUID:-5000}
+PGID=${PGID:-5000}
 
 id dovecot 2>/dev/null
 [ ! $? -eq 0 ] && addgroup -g $PGID dovecot && adduser -u $PUID -G dovecot -D -s /bin/sh dovecot
 
 if [ ! "$(id -u dovecot)" -eq "$PUID" ]; then usermod -o -u "$PUID" dovecot ; fi
 if [ ! "$(id -g dovecot)" -eq "$PGID" ]; then groupmod -o -g "$PGID" dovecot ; fi
-
+mkdir /config
+echo "${CIVIMAIL_USER}:{plain}${CIVIMAIL_PASS}:${PUID}:${GUID}::/civimail" > /config/dovecot.passwd
 chown -R dovecot:dovecot /config
-chown dovecot:dovecot /mail
-chmod +x /mail
 
 if [ ! -f "/config/dovecot.passwd" ]; then
   die "/config/dovecot.passwd does not exist - please create it. an example: 
